cbc207333aa88fa3074ef4623e564e99437d0fbc
Naman <naman532002@gmail.com>
2026-06-30 14:17:13 +0530

    changes

--- utils/retriever.py
+++ utils/retriever.py
@@ -9,27 +9,37 @@ def retrieve_relevant_chunks(index, chunks, question, top_k=3):
     """
 
     try:
+
         embedding_model = load_embedding_model()
 
-        # This line of code will converts sentence into a vector:
         question_embedding = embedding_model.encode([question])
 
         question_embedding = np.array(question_embedding).astype("float32")
 
-    # FAISS performs similarity search. Ex: FAISS finds: Chunk 8, Chunk 15 and Chunk 22, because those chunks are semantically closest.
-        distances, indices = index.search(question_embedding, top_k)
+        distances, indices = index.search(
+            question_embedding,
+            top_k
+        )
 
         results = []
 
-        # FAISS returns positions.Ex: [8, 15, 22], We convert those positions back into actual text chunks.
-        for idx in enumerate(indices[0]):
-            if idx != -1:
-                results.append({
+        for i, idx in enumerate(indices[0]):
+
+            if idx == -1:
+                continue
+
+            results.append(
+                {
                     "content": chunks[idx],
                     "chunk_id": int(idx),
-                    "score": float(distances[0][1])
-                    })
+                    "score": float(distances[0][i])
+                }
+            )
+
         return results
-    
+
     except Exception as e:
-        raise Exception(f"Error retrieving chunks: {str(e)}")
+
+        raise Exception(
+            f"Error retrieving chunks: {str(e)}"
+        )
